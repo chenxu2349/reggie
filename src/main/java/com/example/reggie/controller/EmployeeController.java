32936365185462304a30d3df17a0af7912d1d3ad
chenxu2349 <chenxu_2349@qq.com>
2022-05-30 19:52:34 +0800

    员工信息分页查询
    Signed-off-by: chenxu2349 <chenxu_2349@qq.com>

--- src/main/java/com/example/reggie/controller/EmployeeController.java
+++ src/main/java/com/example/reggie/controller/EmployeeController.java
@@ -5,8 +5,11 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.reggie.common.Result;
 import com.example.reggie.pojo.Employee;
 import com.example.reggie.service.EmployeeService;
+import com.example.reggie.service.impl.EmployeeServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,7 +22,7 @@ import java.time.LocalDateTime;
 @RequestMapping("/employee")
 public class EmployeeController {
 
-    //注入service接口
+
     @Autowired
     private EmployeeService employeeService;
 
@@ -96,13 +99,25 @@ public class EmployeeController {
     }
 
     /**
-     * 员工信息列表分页查询
+     * 员工信息列表分页查询，使用MP的一些方法
      * chenxu
      * 2022/5/28 21:43
      **/
     @GetMapping("/page")
     public Result<Page> page(int page, int pageSize, String name){
-        return null;
+        log.info("page={},pageSize={},name={}",page,pageSize,name);
+
+        //构造分页构造器
+        Page pageInfo = new Page(page,pageSize);
+        //构造条件构造器（name可能会传过来）
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
+        //过滤条件
+        queryWrapper.like(StringUtils.isNotEmpty(name),Employee::getName,name);
+        //排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+        //执行查询
+        employeeService.page(pageInfo,queryWrapper);
+        return Result.success(pageInfo);
     }
 
 }
--- src/main/java/com/example/reggie/service/EmployeeService.java
+++ src/main/java/com/example/reggie/service/EmployeeService.java
@@ -1,7 +1,15 @@
 package com.example.reggie.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.reggie.common.Result;
 import com.example.reggie.pojo.Employee;
 
 public interface EmployeeService extends IService<Employee> {
+//    /**
+//     *@Description 员工分页查询
+//     *@Author chenxu
+//     *@Date 2022/5/29 22:24
+//     **/
+//    public Result<Page> page(int page, int pageSize, String name);
 }
